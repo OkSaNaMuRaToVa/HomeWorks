549e6f33ddce62235bd72a9a237641175ec4f759
unknown <you@example.com>
2021-07-27 23:04:12 +0300

    js_6

---
+++ js/js_6/js/js_3.js
@@ -0,0 +1,7 @@
+const getUsersWithGender = (users, gender) => {
+  
+  };
+  
+  console.log(getUsersWithGender(users, 'Moore Hensley'));
+  console.log(getUsersWithGender(users, 'Ross Vazquez'));
+  console.log(getUsersWithGender(users, 'Carey Barr'));
---
+++ js/js_6/js/js_6.js
@@ -0,0 +1,7 @@
+const getUsersWithAge = (users, min, max) => {
+  
+  };
+  
+  console.log(getUsersWithAge(users, 40, 50)); 
+  
+  console.log(getUsersWithAge(users, 50, 60));
